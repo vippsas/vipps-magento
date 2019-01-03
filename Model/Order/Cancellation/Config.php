1e02f6d2802b38e375eb8066d4e1dfa81d61112a
Oleg Malichenko <oleg.malichenko@vaimo.com>
2019-01-03 13:09:26 +0200

    VIPPS-182: Add order cancellation configuration
    Add paypal module dependency.

---
+++ Model/Order/Cancellation/Config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright 2018 Vipps
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ *  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ *  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ *
+ */
+
+namespace Vipps\Payment\Model\Order\Cancellation;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Config
+ */
+class Config
+{
+    /**
+     * @const string
+     */
+    const XML_PATH_TYPE = 'vipps/order_cancellation/type';
+
+    /**
+     * @const string
+     */
+    const XML_PATH_ATTEMPT_COUNT = 'vipps/order_cancellation/attempts_count';
+
+    /**
+     * @const string
+     */
+    const XML_PATH_INACTIVITY_TIME = 'vipps/order_cancellation/customer_inactivity_time';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Cancellation type code.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->getStoreConfig(self::XML_PATH_TYPE);
+    }
+
+    /**
+     * Common method to return store config value.
+     *
+     * @param $path
+     * @return mixed
+     */
+    private function getStoreConfig($path)
+    {
+        return $this->scopeConfig->getValue($path, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Number of failed attempts.
+     *
+     * @return int
+     */
+    public function getAttemptsCount()
+    {
+        return $this->getStoreConfig(self::XML_PATH_ATTEMPT_COUNT);
+    }
+
+    /**
+     * Return inactivity time in minutes.
+     *
+     * @return int
+     */
+    public function getInactivityTime()
+    {
+        return $this->getStoreConfig(self::XML_PATH_INACTIVITY_TIME);
+    }
+}
