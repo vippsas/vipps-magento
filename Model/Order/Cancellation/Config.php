45e20c1f2163f32449b6074eee9ccae8d7375f31
Oleg Malichenko <oleg.malichenko@vaimo.com>
2019-04-26 15:35:43 +0300

    VIPPS-223: Publication v.1.2.7
    - Create uninstall script.
    - Simplify cancellation config path.

--- Model/Order/Cancellation/Config.php
+++ Model/Order/Cancellation/Config.php
@@ -29,22 +29,22 @@ class Config
     /**
      * @const string
      */
-    const XML_PATH_TYPE = 'vipps/order_cancellation/type';
+    const XML_PATH_TYPE = 'payment/vipps/cancellation/type';
 
     /**
      * @const string
      */
-    const XML_PATH_ATTEMPT_COUNT = 'vipps/order_cancellation/attempts_count';
+    const XML_PATH_ATTEMPT_COUNT = 'payment/vipps/cancellation/attempts_count';
 
     /**
      * @const string
      */
-    const XML_PATH_INACTIVITY_TIME = 'vipps/order_cancellation/customer_inactivity_time';
+    const XML_PATH_INACTIVITY_TIME = 'payment/vipps/cancellation/customer_inactivity_time';
 
     /**
      * @const string
      */
-    const XML_PATH_QUOTE_STORAGE_PERIOD = 'vipps/order_cancellation/quote_storage_period';
+    const XML_PATH_QUOTE_STORAGE_PERIOD = 'payment/vipps/cancellation/quote_storage_period';
 
     /**
      * @var ScopeConfigInterface
