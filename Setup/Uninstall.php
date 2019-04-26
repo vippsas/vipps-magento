e8b0671d66b5de8c23891fde09fd8e0e79ea46df
Oleg Malichenko <aluminium1989@yandex.ru>
2019-04-26 16:03:31 +0300

    Publication 1.3.6
    - Create uninstall script.
    - Simplify cancellation config path.

---
+++ Setup/Uninstall.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright 2019 Vipps
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+namespace Vipps\Payment\Setup;
+
+use Magento\Config\Model\ResourceModel\Config;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+
+/**
+ * Class Uninstall
+ */
+class Uninstall implements UninstallInterface
+{
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * Uninstall constructor.
+     * @param Config $config
+     */
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Invoked when remove-data flag is set during module uninstall.
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        /** @var AdapterInterface $connection */
+        $connection = $setup->getConnection();
+        $tables = ['vipps_payment_jwt', 'vipps_profiling', 'vipps_quote_attempt', 'vipps_quote'];
+
+        array_map(function ($table) use ($connection) {
+            $connection->dropTable($table);
+        }, $tables);
+
+        $tableName = $this->config->getMainTable();
+        $select = $connection
+            ->select()
+            ->from($tableName)
+            ->where('path like "%?%"', 'payment/vipps');
+
+        $connection->deleteFromSelect($select, $tableName);
+
+    }
+}
