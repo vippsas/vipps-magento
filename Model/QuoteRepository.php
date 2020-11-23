3e229b29e141b5be50cdc35264a42205973f395f
Eduard Melnyk <edmelnyk@ukr.net>
2020-11-23 10:52:00 +0200

    Vipps Payment 2.4.5 (#82)
    - improved linking order ids between Vipps and Magento
    - fixed issue with payment status for all orders

--- Model/QuoteRepository.php
+++ Model/QuoteRepository.php
@@ -16,8 +16,12 @@
 
 namespace Vipps\Payment\Model;
 
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Psr\Log\LoggerInterface;
 use Vipps\Payment\Api\Data\QuoteInterface;
 use Vipps\Payment\Api\QuoteRepositoryInterface;
 use Vipps\Payment\Model\QuoteFactory;
@@ -39,15 +43,41 @@ class QuoteRepository implements QuoteRepositoryInterface
     private $quoteFactory;
 
     /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * QuoteRepository constructor.
      *
      * @param QuoteResource $quoteResource .
      * @param QuoteFactory $quoteFactory
+     * @param OrderRepositoryInterface $orderRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param LoggerInterface $logger
      */
-    public function __construct(QuoteResource $quoteResource, QuoteFactory $quoteFactory)
-    {
+    public function __construct(
+        QuoteResource $quoteResource,
+        QuoteFactory $quoteFactory,
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        LoggerInterface $logger
+    ) {
         $this->quoteResource = $quoteResource;
         $this->quoteFactory = $quoteFactory;
+        $this->orderRepository = $orderRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->logger = $logger;
     }
 
     /**
@@ -57,13 +87,13 @@ class QuoteRepository implements QuoteRepositoryInterface
      * @return QuoteInterface
      * @throws CouldNotSaveException
      */
-    public function save(QuoteInterface $quote)
+    public function save(QuoteInterface $quote): QuoteInterface
     {
         try {
             $this->quoteResource->save($quote);
-
             return $quote;
         } catch (\Exception $e) {
+            $this->logger->error($e);
             throw new CouldNotSaveException(
                 __(
                     'Could not save Vipps Quote: %1',
@@ -82,7 +112,7 @@ class QuoteRepository implements QuoteRepositoryInterface
      * @return Quote
      * @throws NoSuchEntityException
      */
-    public function loadByQuote($quoteId)
+    public function loadByQuote($quoteId): QuoteInterface
     {
         $vippsQuote = $this->quoteFactory->create();
         $this->quoteResource->load($vippsQuote, $quoteId, 'quote_id');
@@ -95,18 +125,33 @@ class QuoteRepository implements QuoteRepositoryInterface
     }
 
     /**
-     * @param string $orderId
+     * @param string $reservedOrderId
      *
      * @return QuoteInterface
      * @throws NoSuchEntityException
      */
-    public function loadByOrderId($orderId): ?QuoteInterface
+    public function loadByOrderId($reservedOrderId): QuoteInterface
     {
         $vippsQuote = $this->quoteFactory->create();
-        $this->quoteResource->load($vippsQuote, $orderId, 'reserved_order_id');
+        $this->quoteResource->load($vippsQuote, $reservedOrderId, 'reserved_order_id');
 
         if (!$vippsQuote->getId()) {
-            throw NoSuchEntityException::singleField('reserved_order_id', $orderId);
+            throw NoSuchEntityException::singleField('reserved_order_id', $reservedOrderId);
+        }
+
+        if (!$vippsQuote->getOrderId()) {
+            $order = $this->tryLocateOrder($reservedOrderId);
+            if ($order) {
+                try {
+                    $vippsQuote->setOrderId((int)$order->getEntityId());
+                    if ($vippsQuote->getStatus() == QuoteInterface::STATUS_NEW) {
+                        $vippsQuote->setStatus(QuoteInterface::STATUS_PENDING);
+                    }
+                    $vippsQuote = $this->save($vippsQuote);
+                } catch (\Throwable $t) {
+                    $this->logger->error($t);
+                }
+            }
         }
 
         return $vippsQuote;
@@ -118,15 +163,32 @@ class QuoteRepository implements QuoteRepositoryInterface
      * @return Quote
      * @throws NoSuchEntityException
      */
-    public function load(int $vippsQuoteId)
+    public function load(int $monitoringQuoteId)
     {
-        $vippsQuote = $this->quoteFactory->create();
-        $this->quoteResource->load($vippsQuote, $vippsQuoteId);
+        $monitoringQuote = $this->quoteFactory->create();
 
-        if (!$vippsQuote->getId()) {
-            throw NoSuchEntityException::singleField('entity_id', $vippsQuoteId);
+        if (!$monitoringQuote->getId()) {
+            throw NoSuchEntityException::singleField('entity_id', $monitoringQuoteId);
         }
 
-        return $vippsQuote;
+        return $monitoringQuote;
+    }
+
+    /**
+     * @param $reservedOrderId
+     *
+     * @return OrderInterface|null
+     */
+    private function tryLocateOrder($reservedOrderId): ?OrderInterface
+    {
+        $this->searchCriteriaBuilder->addFilter(OrderInterface::INCREMENT_ID, $reservedOrderId);
+        $criteria = $this->searchCriteriaBuilder->create();
+        $orders = $this->orderRepository->getList($criteria)->getItems();
+
+        if (empty($orders)) {
+            return null;
+        }
+
+        return current($orders);
     }
 }
