5f7b27dac367e624d0d4327f2b62578dcc7cb3a3
Volodymyr Klymenko <volodymyr.klymenko@vaimo.com>
2021-03-18 11:07:15 +0200

    VIPPS-372: Magento 2.4: Add 'Restore a quote' functionality

--- Model/TransactionProcessor.php
+++ Model/TransactionProcessor.php
@@ -302,7 +302,6 @@ class TransactionProcessor
             throw new AcquireLockException(
                 __('Can not acquire lock for order "%1"', $reservedOrderId)
             );
-
         }
 
         return $lockName;
