eb9655a7535a5eb19245eabfedfd2e505fa78678
Joona Melartin <joona.melartin@vaimo.com>
2025-04-17 14:13:25 +0300

    VIPPS-465: Fixing test namespaces

--- Test/Integration/Controller/Payment/FallbackTest.php
+++ Test/Integration/Controller/Payment/FallbackTest.php
@@ -16,7 +16,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-namespace Vipps\Payment\Test\Integration\Model;
+namespace Vipps\Payment\Test\Integration\Controller\Payment;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Checkout\Model\Session;
